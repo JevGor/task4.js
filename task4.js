df38781903718dd74013675b524a1daf39fdeab9
Jev <jev@Jevs-MacBook-Pro.local>
2019-02-22 22:20:32 +0100

    first commit

---
+++ task4.js
@@ -0,0 +1,19 @@
+var a  =  10, b  =  2;
+
+console.log (a+b)
+console.log (a-b)
+console.log (a*b)
+console.log (a/b)
+
+var c  =  15, d  =  2;
+result = a+b;
+console.log (result)
+
+var a  =  10, b  =  2, c  =  5;
+console.log (a+b+c)
+
+var a  =  17, b  = 10;
+c  =  a - b; 
+var d  =  7;
+result  =  c + d;
+console.log (result)
